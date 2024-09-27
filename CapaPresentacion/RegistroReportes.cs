0fbf929f9ca7dcbffc72e774a168a4dfa053d7e8
Marcelo Leoncio Diaz Sifuentes <Choscreator1212@gmail.com>
2024-09-27 17:21:11 -0500

    Registro de asistencias a Registro de Reportes

---
+++ CapaPresentacion/RegistroReportes.cs
@@ -0,0 +1,78 @@
+﻿using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDato;
+using ClosedXML.Excel;
+
+
+namespace CapaPresentacion
+{
+    public partial class Registro_E_S : Form
+    {
+        private EmpleadoCN empleadoCN = new EmpleadoCN();
+        public Registro_E_S()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Registro_E_S_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+       
+
+        private void btnBuscarRegistro_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+           
+        }
+        
+        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        
+    }
+}
